d9ddde56626440f227a037848799d9ef254a8c84
AhmadAl-Habal <80961574+AhmadAl-Habal@users.noreply.github.com>
2024-12-29 19:23:55 +0300

    add hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import logo from "../assets/logo.png";
+import hero from "../assets/hero.webp";
 import { Link } from "react-router-dom";
-Link
+Link;
 const Hero = () => {
   return (
     <>
-      {" "}
+     
       <section className="py-2 mb-4 flex justify-center">
-        <img className="w-full h-[200px] max-w-[500px]" src={logo} alt="" />
-      </section>{" "}
-      <div className="block"><Link to={"/add-product"}>Add new Product</Link></div>
+        <img className="w-full h-[300px]" src={hero} alt="" />
+      </section>
+      <div className="block">
+        <Link to={"/add-product"}>Add new Product</Link>
+      </div>
     </>
   );
 };
