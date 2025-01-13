59fd8b1a1428bd1fdcc406249c2f84478825140e
AhmadAl-Habal <80961574+AhmadAl-Habal@users.noreply.github.com>
2025-01-13 16:40:03 +0300

    test

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 // const baseURL =
 //   process.env.NODE_ENV === 'production'
 //     ? process.env.PROD_DATABASE_URL
 //     : process.env.DEV_DATABASE_URL;
 
-    const baseURL = import.meta.env.VITE_DATABASE_URL;
+// const baseURL = import.meta.env.VITE_DATABASE_URL;
 
 const axiosInstance = axios.create({
-  baseURL,
-  timeout: 10000, 
+  baseURL: "https://abomariambackend.vercel.app/",
+  timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 // dev env https://abomariamvapestorebackend.onrender.com
