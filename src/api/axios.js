95c85a21b403edf5dcd4b13ce1df04879811fc5d
AhmadAl-Habal <80961574+AhmadAl-Habal@users.noreply.github.com>
2025-01-20 10:11:35 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://abomariamvapestorebackend.onrender.com/',
-  timeout: 10000, 
+  baseURL: "https://abomariambackend.vercel.app/",
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
