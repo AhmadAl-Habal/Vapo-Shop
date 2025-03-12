ee098440cf41ab712f0ec57018d7ed85f024ab4f
AhmadAl-Habal <80961574+AhmadAl-Habal@users.noreply.github.com>
2025-03-12 06:29:34 +0300

    change welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -46,8 +46,10 @@ const WelcomeSpinner = ({ motionBg }) => {
 
       {/* Animated Content */}
       <motion.div
-        animate={{ scale: [0, 1.05, 1], opacity: [0.8, 0.9, 0.8] }}
-        transition={{ duration: 1.5, repeat: Infinity }}
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+        whileHover={{ scale: 1.05 }}
         className="relative h-screen flex items-center justify-center text-center px-4 mt-10"
       >
         <div className="text-white">
